2b16b1c952f96f1b8f5a68fa36fb17b543d7019c
muiky05 <77947877+muiky05@users.noreply.github.com>
2022-12-12 22:05:17 -0500

    Update gelatest.lua

--- gelatest.lua
+++ gelatest.lua
@@ -14,7 +14,7 @@ Global.GelatekHubConfig = {
 	["Anti Void Offset"] = 75
 }
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Gelatekussy/GelatekHub/main/src/lib/UILibrary.lua"))()
-local Main = Library:Create("Gelatek Hub", "Revamped by Ernad <3", Color3.fromRGB(159, 1, 198))
+local Main = Library:Create("Gelatek Hub - Test Edition", "Revamped by Ernad <3", Color3.fromRGB(217, 61, 62))
 local UserInputService = game:GetService("UserInputService")
 if UserInputService.TouchEnabled then
 	game.StarterGui:SetCore("SendNotification",{
