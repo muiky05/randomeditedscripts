a473b9c93c5eabac5cd3d8ae5f260dbd1751a9f9
muiky05 <77947877+muiky05@users.noreply.github.com>
2024-11-25 17:31:22 -0500

    Create scripthub.lua

---
+++ scripthub.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 16732694052 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/autofish.lua'))()
+elseif game.PlaceId == 13438553315 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/decayingwinter.lua'))()
+end
