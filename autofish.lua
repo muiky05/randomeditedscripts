0f415a4320e7087fcffb9940150a9b09457c9508
muiky05 <77947877+muiky05@users.noreply.github.com>
2024-11-26 18:23:16 -0500

    Update autofish.lua

--- autofish.lua
+++ autofish.lua
@@ -36,7 +36,7 @@ for i = 1, #wlTable do
 		wl = true
 	end
 end
-if wl = false then LocalPlayer:Kick("You are not whitelisted. Contact me if this is a mistake.") end
+if wl == false then LocalPlayer:Kick("You are not whitelisted. Contact me if this is a mistake.") end
 
 function ExportValue(arg1, arg2)
 	return tonumber(string.format("%."..(arg2 or 1)..'f', arg1))
