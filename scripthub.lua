d7df5529470413c0041d3341fb3cd112a51769a3
muiky05 <77947877+muiky05@users.noreply.github.com>
2024-12-21 18:39:50 -0500

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -2,4 +2,6 @@ if game.PlaceId == 16732694052 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/autofish.lua'))()
 elseif game.PlaceId == 13438553315 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/decayingwinter.lua'))()
+elseif game.PlaceId == 11157166793 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/antwar.lua'))()
 end
