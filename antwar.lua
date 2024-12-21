0f35f46ecfbc8e4bd56a234c5cdfeb4775763591
muiky05 <77947877+muiky05@users.noreply.github.com>
2024-12-21 18:38:36 -0500

    Create antwar.lua

---
+++ antwar.lua
@@ -0,0 +1,50 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/KadeTheExploiter/Uncategorized-Scripts/main/UI-Libraries/Bloom/UI.lua"))()
+local Main = Library:Create("Ernads Autofish")
+local TabH = Main.MakeTab("Ernads Ant War GUI - Welcome, "..LocalPlayer.Name, 6023426922)
+
+
+local Sections = {
+    ['Home'] = {
+        Main = TabH.MakeSection("Main")
+    }
+}
+
+local Main = Sections.Home.Main
+local killaura
+
+Automation.Toggle("Auto-Attack (+Attack Speed)", false, function(Bool)
+	killaura = Bool
+end)
+
+local function findClosestPlayer()
+    local myPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+    local closestPlayer = nil
+    local shortestDistance = math.huge
+
+    for _, player in pairs(game.Players:GetPlayers()) do
+        if player ~= game.Players.LocalPlayer then
+            local character = player.Character
+            if character and character:FindFirstChild("HumanoidRootPart") then
+                local distance = (myPosition - character.HumanoidRootPart.Position).magnitude
+                if distance < shortestDistance then
+                    shortestDistance = distance
+                    closestPlayer = player
+                end
+            end
+        end
+    end
+end
+
+while task.wait(0.1) do
+  local closestPlayer = findClosestPlayer()
+    if closestPlayer then
+    	local args = {
+    	    [1] = "Bite",
+    	    [2] = closestPlayer.Character.Humanoid,
+    	    [3] = closestPlayer.Character.BiteHitbox
+    	}
+      if killaura == true then
+    	  game:GetService("ReplicatedStorage").ServerEvents.Bite:FireServer(unpack(args))
+      end
+    end
+end
