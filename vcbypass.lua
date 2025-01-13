5581c0bd302d91fba63029f77f1577e39f32f18f
muiky05 <77947877+muiky05@users.noreply.github.com>
2025-01-13 15:50:06 -0500

    Create vcbypass.lua

---
+++ vcbypass.lua
@@ -0,0 +1,36 @@
+local Players = game:GetService('Players')
+local VoiceChatService = game:GetService('VoiceChatService')
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/KadeTheExploiter/Uncategorized-Scripts/main/UI-Libraries/Bloom/UI.lua"))()
+
+local LocalPlayer = Players.LocalPlayer
+local wl
+
+local wlTable = {
+"muiky05",
+}
+
+for i = 1, #wlTable do
+	if (wlTable[i] == value) then
+		wl = true
+	end
+end
+if wl == false then LocalPlayer:Kick("You are not whitelisted. Contact me if this is a mistake.") end
+
+local Main = Library:Create("Ernads VC Bypasser")
+local TabH = Main.MakeTab("Ernads VC Bypasser - Welcome, "..LocalPlayer.Name, 6023426922)
+
+
+local Sections = {
+    ['Home'] = {
+        Bypass = TabH.MakeSection("Bypass"),
+    }
+}
+
+local Automation = Sections.Home.Automation
+
+Bypass.Button("Unban VC", function()
+  VoiceChatService:joinVoice()
+end)
+
+Bypass.Label("Made by Keozog on Discord.")
