0f3befc241c7e6f1af7d7ee4386cf4aed8e43c4f
muiky05 <77947877+muiky05@users.noreply.github.com>
2025-01-13 23:56:37 -0500

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -1,15 +1,9 @@
-if menutype then
-  if menutype == "bothub" then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/bot.lua'))()
-  elseif menutype == "vcbypass" then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/vcbypass.lua'))()
-  end
+if game.PlaceId == 16732694052 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/autofish.lua'))()
+elseif game.PlaceId == 13438553315 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/decayingwinter.lua'))()
+elseif game.PlaceId == 11157166793 then
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/antwar.lua'))()
 else
-  if game.PlaceId == 16732694052 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/autofish.lua'))()
-  elseif game.PlaceId == 13438553315 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/decayingwinter.lua'))()
-  elseif game.PlaceId == 11157166793 then
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/antwar.lua'))()
-  end
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/vcbypass.lua'))()
 end
