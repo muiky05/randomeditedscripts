23de6e68a1476bc926ce6ba2d9ea2ec612aa29c4
muiky05 <77947877+muiky05@users.noreply.github.com>
2025-02-13 20:53:17 -0500

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -1,3 +1,15 @@
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+local HS = game:GetService("HttpService")
+local WebhookURL = "https://webhook.lewisakura.moe/api/webhooks/1339775663472640164/VZRhlXstsTTenw7fBpxPjWyyjhGgdKNmEp4orSaRU3Aos5eWalDFpgf3aEQJj69wP2Mu" --if ur somehow here, you can go ahead and wreck the webhook, props to you, but i will end up just making a new one
+--why target me anyways? lol go ham tho
+
+local MessageData = {
+	["content"] = game.Players.LocalPlayer.Name .. "ran the hub in " .. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name .. " (" .. tostring(game.PlaceId) .. ")"
+}
+MessageData = HS:JSONEncode(MessageData)
+HS:PostAsync(WebhookURL,MessageData)
+
 if game.PlaceId == 16732694052 then
   loadstring(game:HttpGet('https://raw.githubusercontent.com/muiky05/randomeditedscripts/refs/heads/main/autofish.lua'))()
 elseif game.PlaceId == 13438553315 then
