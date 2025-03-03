46dc858570a9bafb67bf4105bf4f06a288c56360
muiky05 <77947877+muiky05@users.noreply.github.com>
2025-03-03 16:56:16 -0500

    Update decayingwinter.lua

--- decayingwinter.lua
+++ decayingwinter.lua
@@ -28,7 +28,7 @@ for i = 1, #wlTable do
 		wl = true
 	end
 end
-if wl == false then LocalPlayer:Kick("You are not whitelisted. Contact me if this is a mistake.") end
+if wl == false then LocalPlayer:Kick("You are not whitelisted. Contact me if this is a mistake.") return end
 
 local Sections = {
     ['Main'] = {
