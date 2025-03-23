642a750aae10b40d4ebae15266ffda89893928c1
muiky05 <77947877+muiky05@users.noreply.github.com>
2025-03-22 22:28:41 -0400

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,3 +1,12 @@
+game:GetService("RunService"):Set3dRenderingEnabled(false)
+
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+    wait(1)
+    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+
 local bootTime = os.time()
 local disconnected = false
 
